9638e0b9d22f400ab85514bda1ce87d4ff7a2e7d
Daisuke Hiroshima <d.hiroshima777@gmail.com>
2022-01-07 21:23:21 +0900

    初回コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import img from './img/sample.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card sx={{ maxWidth: 750 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={`${process.env.PUBLIC_URL}/img/sample.jpg`}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              サンプル
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              これからapiでデータを取得して表示していきます。
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </header>
     </div>
   );
